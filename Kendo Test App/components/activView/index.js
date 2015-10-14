8ef14085350a01e66ef9506d171b77bf3e7d2a96
Peter De Saegher <pds@organi.be>
2015-10-14 10:28:13 +0200

    pds alternatieve index-pagina

--- Kendo Test App/components/activView/index.js
+++ Kendo Test App/components/activView/index.js
@@ -1,5 +1,26 @@
 'use strict';
 
+//START_CUSTOM_CODE_activView
+var activModel = kendo.data.Model.define({
+	fields: {
+		"firmanr": {type: "number"},
+		"activnr": {type: "number"},
+		"sessienr": {type: "number"},
+		"datumact": {type: "string"},
+		"cRowId": {type: "string"},
+		"naamact": {type: "string"}
+	}
+});
+
+var dataLocal = [];
+
+dataLocal.push({cRowId: "1", firmanr: 1, activnr: 1, sessienr: 1, datumact: "05/09/2015" });
+dataLocal.push({cRowId: "2", firmanr: 1, activnr: 2, sessienr: 1, datumact: "07/09/2015" });
+dataLocal.push({cRowId: "3", firmanr: 1, activnr: 3, sessienr: 1, datumact: "08/09/2015" });
+dataLocal.push({cRowId: "4", firmanr: 1, activnr: 4, sessienr: 1, datumact: "09/09/2015" });
+
+console.log("array init finished");
+
 app.activView = kendo.observable({
     onShow: function() {},
     afterShow: function() {},
@@ -13,60 +34,16 @@ app.activView = kendo.observable({
     		}       		
     	},
     	schema: {
-    		model:{
-    			id: "cRowId",
-    			fields: {
-    				firmanr: {type: "number"},
-    				activnr: {type: "number"},
-    				sessienr: {type: "number"},
-    				datumact: {type: "string"},
-    				cRowId: {type: "string"}
-    				
-    			}
-    			
-    		}                		
-    	},
-    	batch: true
-    
+    		model: activModel,   
+    		data: "activiteiten"
+    	}    
     	
     }),
-    ActivLocal: new kendo.data.DataSource({
-    	data: [
-    	       {cRowId: "1", firmanr: 1, activnr: 1, sessienr: 1, datumact: "05/09/2015" },
-    	       {cRowId: "2", firmanr: 1, activnr: 2, sessienr: 1, datumact: "07/09/2015" },
-    	       {cRowId: "3", firmanr: 1, activnr: 3, sessienr: 1, datumact: "09/09/2015" },
-    	       {cRowId: "4", firmanr: 1, activnr: 4, sessienr: 1, datumact: "10/09/2015" }    	
-    	],
-    	schema: {
-            model: {
-                id: "cRowId",
-                fields: {
-                    firmanr: {
-                        type: "number"
-                    },
-                    activnr: {
-                        type: "number"
-                    },
-                    sessienr: {
-                        type: "number"
-                    },
-                    datumact: {
-                        type: "string"
-                    },
-                    cRowId: {
-                        type: "string"
-                    }
-
-                }
-
-            }
-
-        }
-    	
-    	
+    ActivLocal: new kendo.data.DataSource({    	
+    	data: dataLocal
     })
     
 });
 
-// START_CUSTOM_CODE_activView
+
 // END_CUSTOM_CODE_activView
--- Kendo Test App/components/customer/index.js
+++ Kendo Test App/components/customer/index.js
@@ -1,8 +1,20 @@
 'use strict';
 
 app.customer = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
+    onShow: function() {   	
+    },
+    afterShow: function() {},
+    
+    dsCustomers: new kendo.data.DataSource({
+    	transport:{
+    		read: { 
+    			url: "http://orwit2008003:8980/pug/rest/pugService/readcustomer",    
+    			type: "get",
+    			dataType: "json"
+    		}      
+    		
+    	}    	
+    })        	    
 });
 
 // START_CUSTOM_CODE_customer
